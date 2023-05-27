707a314e25eb94ce7e116eab81ce9f89f0e12648
tonson <tonson.pa@gmail.com>
2023-05-27 11:58:10 +0700

    use cowsay

--- go-module/main.go
+++ go-module/main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"fmt"
 	"soda-meow/surprise"
+
+	cowsay "github.com/Code-Hex/Neo-cowsay/v2"
 )
 
 func main() {
 	fmt.Println("this is main")
 	fmt.Println(sayMeow())
 	fmt.Println(sayMeowSurprise())
+
+	say, err := cowsay.Say(
+		"Hello",
+		cowsay.Type("default"),
+		cowsay.BallonWidth(40),
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(say)
 }
 
 func sayMeowSurprise() string {
